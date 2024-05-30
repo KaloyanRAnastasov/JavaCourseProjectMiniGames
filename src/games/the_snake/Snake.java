edd238ef063e66bafb7798cb40552bd19e5e0188
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-05-30 19:51:57 +0300

    Резолюцията на играта вече е оправена спрямо матрицата в нея и също така са подобрени и изяснени тесотвите функции на играта в package Test.

--- src/games/the_snake/Snake.java
+++ src/games/the_snake/Snake.java
@@ -12,7 +12,7 @@ public class Snake extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY = 90;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
